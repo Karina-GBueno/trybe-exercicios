271d9e0e2587b8cc8bae82e630ab5e2dafc13dc7
Karina-GBueno <karinabueno.dev@gmail.com>
2023-05-04 11:58:29 -0300

    Introdução a Funções

--- Fundamentos/secao-03-introducao-a-javascript/dia -02-array-e-loop-for/exercicio-02/scripts.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia -02-array-e-loop-for/exercicio-02/scripts.js
@@ -1,11 +1,3 @@
-/*
-
-
-
-
-9- Utilizando for, crie um array que vá de 1 a 25 e imprima o resultado.
-10- Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.*/
-
 /*1- Percorra o array imprimindo todos os valores contidos nele com a função console.log().*/
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
@@ -13,7 +5,7 @@ for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 }
 
-console.log('---------------------');
+console.log("---------------------");
 
 /*2- Some todos os valores contidos no array e imprima o resultado.*/
 
@@ -25,7 +17,7 @@ for (let index = 0; index < numbers.length; index += 1) {
 
 console.log(sum);
 
-console.log('---------------------');
+console.log("---------------------");
 
 /*3- Calcule e imprima a média aritmética dos valores contidos no array.*/
 
@@ -33,17 +25,17 @@ let arithmetic = sum / numbers.length;
 
 console.log(arithmetic);
 
-console.log('---------------------');
+console.log("---------------------");
 
 /*4- Com base no código que acabou de gerar, referente ao calculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.*/
 
 if (sum > 10) {
-  console.log('O valor da média aritmética é maior que 20');
+  console.log("O valor da média aritmética é maior que 20");
 } else {
-  console.log('O valor da média aritmética é menos ou igual a 20');
+  console.log("O valor da média aritmética é menos ou igual a 20");
 }
 
-console.log('---------------------');
+console.log("---------------------");
 
 /*6- Utilizando for, descubra o maior valor contido no array e imprima-o.*/
 
@@ -51,27 +43,65 @@ let highestValue = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
   if (numbers[index] > highestValue) {
-   highestValue = numbers[index];
-  }  
+    highestValue = numbers[index];
+  }
 }
 console.log(highestValue);
 
-console.log('---------------------');
+console.log("---------------------");
 
 /*7- Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.*/
 let result = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
   if (numbers[index] % 2 !== 0) {
-  result += 1;
-  } 
+    result += 1;
+  }
 }
 
 if (result === 0) {
-  console.log('Nenhum valor impar encontrado.');
+  console.log("Nenhum valor impar encontrado.");
 } else {
   console.log(result);
 }
 
+console.log("---------------------");
+
+/*9- Utilizando for, crie um array que vá de 1 a 25 e imprima o resultado.*/
+
+let num = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  num.push(index);
+}
+console.log(num);
+
+console.log("---------------------");
+
+/*10- Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.*/
+
+for (let index = 1; index <= 25; index += 1) {
+  console.log(index / 2);
+}
+
 console.log('---------------------');
 
+/*crie um algoritmo que imprima na tela o fatorial de 10.*/
+
+let factorial = 10;
+
+for (let index = 10; index > 0; index -= 1) {
+  factorial = factorial * index;
+}
+console.log(factorial);
+
+console.log('---------------------');
+
+/*Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra, como a palavra “banana” para “ananab”. Utilize a string abaixo como exemplo. Depois, troque-a por outras para verificar se seu algoritmo está funcionando corretamente.*/
+
+// let reverse = 'trybe';
+
+// for (let index = 1; index = reverse.length; index -= 1) {
+//   reverse.push(index);
+// }
+// console.log(reverse);
